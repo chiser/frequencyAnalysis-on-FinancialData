9dd59e46f1f2d58ead437b809d384dfa8df106ab
Christian Rohrsen <christian.c.rohrsen@gmail.com>
2020-05-30 14:00:55 +0200

    finished the first sketch

--- analysis.R
+++ analysis.R
@@ -1,39 +1,46 @@
 # load quantmod, then some data, and charts away...
 library(quantmod)
-getSymbols("IBM")
 
-barChart(IBM)
-candleChart(IBM)
-lineChart(IBM)
+getSymbols(c('QQQ','SPY',"IBM","AAPL"))
 
-data<-as.data.frame(IBM$IBM.Close)
-time<-index(data)
-time<-1:length(data)
-# index(A[diff(A)<0])
+# barChart(IBM)
+# candleChart(IBM)
+# lineChart(IBM)
+
+aktie_ts<- SPY$SPY.Close #QQQ$QQQ.Close #AAPL$AAPL.Close  #IBM$IBM.Close
+
+data<-as.data.frame(aktie_ts)
+# time<-index(data)
+time<-1:nrow(data[1])
+
 
 ### Start with EMD
 
 library (EMD)
 library (hht)
 
-ee <- EEMD (data$IBM.Close,time,250, 10, 6, "trials")
-eec <- EEMDCompile ("trials", 10, 6)
+ee <- EEMD (data[,1],time,250, 10, 10, "trials")
+eec <- EEMDCompile ("trials", 10, 10)
 
-plot (eec$tt, eec$averaged.imfs[,6], type="l")
-lines (eec$averaged.imfs[,1], col="red")
+plot (eec$tt, eec$averaged.imfs[,1], type="l")
 lines (eec$averaged.imfs[,2], col="blue")
 lines (eec$averaged.imfs[,3], col="grey")
 lines (eec$averaged.imfs[,4], col="green")
 lines (eec$averaged.imfs[,5], col="orange")
+lines (eec$averaged.imfs[,6], col="red")
+lines (eec$averaged.imfs[,7], col="yellow")
+lines (eec$averaged.imfs[,8], col="darkblue")
+lines (eec$averaged.imfs[,9], col="darkgreen")
+lines (eec$averaged.imfs[,10], col="darkred")
 
 # Let’s do the same thing with SSA:
 library (Rssa)
 # Decompose series with default parameters
-s <- ssa(data$IBM.Close) #, kind = "mssa"
+s <- ssa(data[,1]) #, kind = "mssa"
 # Show the summary
 summary(s)
 # Reconstruct the series, with suitable grouping
-r <- reconstruct(s, groups = list(c(1, 4), c(2, 3), c(5, 6))) #groups = list(Trend = 1:2)
+r <- reconstruct(s) #groups = list(Trend = 1:2) , groups = list(Trend= c(1, 2), c(3, 4), c(5, 6))
 plot(r)
 
 # Plot original series, trend and residuals superimposed
@@ -42,21 +49,23 @@ plot(r, plot.method = "xyplot", superpose = TRUE,
      col = c("blue", "green", "red", "violet"),
      lty = c(rep(1, 4), rep(2, 4), rep(3, 4)))
 
-# lines (gas.rec$T, col="red")
-
 # FFT
-
-fftData<-fft(data$IBM.Close)
-
-plot(fftData)
-
+# fftData<-fft(data[,1])
+# 
+# plot(fftData)
 
 ## Check the spectrum
 library(seewave)
 
-op<-par(mfrow=c(2,2))
-spec(data$IBM.Close,f=22050)
-spec(data$IBM.Close,f=22050,col="red",plot=2)
-spec(data$IBM.Close,f=22050,dB="max0",col="blue")
-spec(data$IBM.Close,f=22050,dB="max0",col="green",plot=2)
-par(op)
+# op<-par(mfrow=c(2,2))
+# spec(data[,1],f=22050)
+# spec(data[,1],f=22050,col="red",plot=2)
+spec(data[,1],f=22050,dB="max0",col="blue")
+# spec(data[,1],f=22050,dB="max0",col="green",plot=2)
+# par(op)
+
+## Spectral
+library(lattice)
+library(spectral)
+
+spectrum(data[,1])
